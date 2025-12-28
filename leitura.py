1d2b51e67f0c2464b79ac9f1ab06469b7612d4ae
Matheus Zanutto Gaspar <matszg@live.com>
2025-12-28 20:56:28 -0300

    test: modify leitura.py for some tests

--- leitura.py
+++ leitura.py
@@ -1,8 +1,18 @@
 from services.datasetService import dataset_completo
+from services.vetorizacaoService import vetorizacao, encode_Y
 
-def prints():
+def print_dataset():
     df = dataset_completo()
-    print(df)
+    return df
+
+def print_vetorizacao():
+    x_tfidf = vetorizacao()
+    print(x_tfidf)
+
+def print_encode_Y():
+    y_encoded = encode_Y()
+    print(y_encoded)
 
 if __name__ == "__main__":
-    prints()
+    #print_vetorizacao()
+    print_encode_Y()
